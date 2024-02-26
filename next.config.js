38ca0846d7aa9ec5148b063a50c20ba9092bfafc
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-02-27 02:23:48 +0700

    fix cors

--- next.config.js
+++ next.config.js
@@ -1,19 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
+    // Pushing external modules to config.externals
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
       bufferutil: "commonjs bufferutil"
     });
 
-    return config;
+    return config; // Return modified config
   },
   images: {
     domains: [
       "uploadthing.com",
       "utfs.io"
     ]
+  },
+  async headers() {
+    return [
+      {
+        // Routes this applies to
+        source: "/api/(.*)",
+        // Headers
+        headers: [
+          // Allow for specific domains to have access or * for all
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.ALLOWED_ORIGIN || "*", // Use ALLOWED_ORIGIN or fallback to *
+          },
+          // Allows for specific methods accepted
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          // Allows for specific headers accepted
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
