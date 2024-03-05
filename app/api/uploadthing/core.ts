16d0da0ed00c6f3f857491f1fe7ebc038de0b1b0
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-03-05 07:56:52 +0700

    Skip auth when upload file

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -6,12 +6,13 @@ const f = createUploadthing();
 const handleAuth = () => {
   const { userId } = auth();
   if (!userId) throw new Error("Unauthorized");
+  console.log("userId", userId);
   return { userId: userId };
 }
 
 export const ourFileRouter = {
   serverImage: f({ image: { maxFileSize: "1MB", maxFileCount: 1 } })
-    .middleware(() => handleAuth())
+    // .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
   messageFile: f(["image", "pdf"])
     .middleware(() => handleAuth())
