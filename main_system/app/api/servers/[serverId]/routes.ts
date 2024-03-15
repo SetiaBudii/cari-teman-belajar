e11be2a43746f09cb55946514d0cd1399f62add4
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-03-16 01:28:14 +0700

    deleted node module

---
+++ main_system/app/api/servers/[serverId]/routes.ts
@@ -0,0 +1,82 @@
+import { NextResponse } from "next/server";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { description, departement, location, topic1, topic2, topic3 } = await req.json();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    let allTopic = [];
+    //Not using topic=none
+    for (const topic of [topic1, topic2, topic3]) {
+      if (topic !== "none") {
+        allTopic.push(topic);
+      }
+    }
+
+    // Find all server topics associated with the specified serverId
+    const allTopics = await db.serverTopic.findMany({
+      where: { serverId: params.serverId },
+    });
+
+    // Update each server topic
+    for (const topic of allTopics) {
+      await db.serverTopic.update({
+        where: { id: topic.id },
+        //use allTopic to update the name of the topics
+        data: { name: allTopic.shift() || "none" },
+      });
+    }
+
+    //update server and its topics
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+      },
+      data: {
+        description,
+        departement,
+        location,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
